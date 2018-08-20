ca64f454ab82e094cc6412f0cca7b13e5ac3bf42
hippocloud <506107396@qq.com>
2018-08-20 18:47:47 +0800

    test

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello hahaha Servlet!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
